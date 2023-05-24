3f0e462f276b74cfed27f0a88e203c65dcf1810e
Prosto-Den <122309719+Prosto-Den@users.noreply.github.com>
2023-05-24 22:35:47 +0300

    Add files via upload
    -minor bugfixes

--- test_boxes/player.py
+++ test_boxes/player.py
@@ -54,6 +54,8 @@ class Player:
         # переменная для скроллинга
         scroll = 0
 
+        col_thresh = 20
+
         # направо
         if self.moving_right:
             dx += self.speed
@@ -74,6 +76,7 @@ class Player:
             self.jump = False
             self.in_air = True
 
+        # стрельба
         if self.fire:
             blt = bullet.Bullet(self.game, self.rect.centerx + self.image.get_width() * 0.6 * self.direction,
                                 self.rect.centery - self.image.get_height() * 0.6, self.direction)
@@ -92,29 +95,52 @@ class Player:
         """Вот тут изменяем переменную для перемещения"""
         dy += self.vel_y
 
+        # проверка на прилипание
+        if pygame.sprite.spritecollide(self, self.game.sticky_group, False):
+            self.sticky = True
+            self.ground = False
+
+        else:
+            self.sticky = False
+
+        # если прилип
+        if self.sticky:
+            dy = 0
+            self.in_air = False
+
+            if self.jump:
+                self.vel_y = -self.jump_force
+                dy = self.vel_y
+
+                self.sticky = False
+                self.in_air = True
+
+        # если уже отлип, но ещё не на земле
+        if not self.sticky and not self.ground:
+            if self.direction < 0 and not self.moving_left:
+                dx += self.speed * 2
+
+            elif self.direction > 0 and not self.moving_right:
+                dx -= self.speed * 2
+
         # проверка стокновения с платформой
         for plat in self.game.platform_list:
-            # по вертикали
-            if plat.rect.colliderect(self.rect.x, self.rect.y + dy, self.image.get_width(), self.image.get_height()):
-                self.in_air = False
+            if plat.rect.colliderect(self.rect.x + dx, self.rect.y, self.image.get_width(), self.image.get_height()):
                 self.ground = True
+                dx = 0
 
-                if self.vel_y < 0:
+            if plat.rect.colliderect(self.rect.x, self.rect.y + dy, self.image.get_width(), self.image.get_height()):
+                if abs((self.rect.top + dy) - plat.rect.bottom) < col_thresh:
                     self.vel_y = 0
                     dy = plat.rect.bottom - self.rect.top
 
-                else:
-                    dy = plat.rect.top - self.rect.bottom
-                    self.on_platform = True
+                elif abs((self.rect.bottom + dy) - plat.rect.top) < col_thresh:
+                    self.rect.bottom = plat.rect.top - 1
+                    dy = 0
+                    self.in_air = False
 
-                if not self.moving_right and not self.moving_left:
-                    dx = plat.dx
-
-            if plat.rect.colliderect(self.rect.x + dx, self.rect.y, self.image.get_width(), self.image.get_height()):
-                if not self.on_platform:
-                    dx = 0
-
-        self.on_platform = False
+                if plat.move_x != 0:
+                    self.rect.x += plat.dx
 
         # проверка на столкновения со стенками/полом
         for tile in self.game.obstacle_list:
@@ -146,41 +172,13 @@ class Player:
                     self.in_air = False
                     self.ground = True
 
-        if pygame.sprite.spritecollide(self, self.game.sticky_group, False):
-            self.sticky = True
-            self.ground = False
-
-        else:
-            self.sticky = False
-
-        if self.sticky:
-            dy = 0
-            self.in_air = False
-
-            if self.jump:
-                self.vel_y = -self.jump_force
-                dy = self.vel_y
-
-                self.sticky = False
-                self.in_air = True
-
-        if not self.sticky and not self.ground:
-            if self.direction < 0 and not self.moving_left:
-                dx += self.speed * 2
-
-            elif self.direction > 0 and not self.moving_right:
-                dx -= self.speed * 2
-
+        # проверка выхода за границу экрана сверху
         if self.rect.top + dy < 0:
             dy = -self.rect.top
 
         # изменяем координаты игрока
         self.rect.x += dx
         self.rect.y += dy
-
-        # включаем скроллинг
-        if self.rect.x >= var.SCROLL_THRESH + 10:
-            self.on_scroll = True
 
         # скроллинг
         if (self.rect.right > var.WIDTH - var.SCROLL_THRESH and var.bg_scroll < var.COLUMNS * var.TILE_SIZE - var.WIDTH) \

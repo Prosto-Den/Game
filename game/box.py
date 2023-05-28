c4133d52d49a9bdde615284feceab86ca6a4bacf
Prosto-Den <122309719+Prosto-Den@users.noreply.github.com>
2023-05-28 11:58:29 +0300

    Add files via upload
    --bug fixes

--- game/box.py
+++ game/box.py
@@ -15,6 +15,9 @@ class Box(pygame.sprite.Sprite):
 
         self.rect = self.image.get_rect(topleft=(x, y))
 
+        # флаг для проверки столкновения с препятствием
+        self.wall = False
+
     # отрисовка коробки в мире
     def draw(self, scroll):
         # добавляем скроллинг
@@ -24,20 +27,30 @@ class Box(pygame.sprite.Sprite):
 
     # метод толкания коробок
     def move(self, dx):
-        # отслеживает, столкнулись со стеной или нет
-        wall = False
+        # флаг на столкновение с противником
+        enemy_check = False
 
+        # обходим все коробки
         for box in self.game.boxes:
+            # проверка на столкновение со стенами
             for tile in self.game.obstacle_list:
                 if tile[1].colliderect(box.rect.x + dx, box.rect.y, box.width, box.height):
-                    wall = True
+                    box.wall = True
                     break
 
-            if box != self and not wall:
+            # проверка на столкновение с противником
+            for enemy in self.game.enemies:
+                if enemy.rect.colliderect(box.rect.x + dx, box.rect.y, box.width, box.height):
+                    enemy_check = True
+                    break
+
+            # проверка на столкновение с другими коробками
+            if box != self and not self.wall:
                 if box.rect.colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
                     dx = box.move(dx)
 
-        if wall:
+        # если столкнулись со стеной или противником
+        if self.wall or enemy_check:
             dx = 0
 
         # меняем координату
